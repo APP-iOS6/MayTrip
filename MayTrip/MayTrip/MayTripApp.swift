6367e18253801f187c6d89410494afc028236d7f
SeungWoo <kangsw1025@naver.com>
2024-11-01 11:22:14 +0900

    create project

---
+++ MayTrip/MayTrip/MayTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  MayTripApp.swift
+//  MayTrip
+//
+//  Created by 강승우 on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MayTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
