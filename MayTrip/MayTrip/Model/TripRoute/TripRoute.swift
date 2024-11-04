0feb3e43b0a68b48839075b817397861b254ef8e
Heecheol_K <hck1104@naver.com>
2024-11-04 15:16:16 +0900

    Feat: 여행 경로 관련 모델 구조체 작성

--- MayTrip/MayTrip/Model/TripRoute/TripRoute.swift
+++ MayTrip/MayTrip/Model/TripRoute/TripRoute.swift
@@ -4,4 +4,18 @@
 //
 //  Created by 강승우 on 11/1/24.
 //
+import Foundation
 
+
+struct TripRoute: Codable{
+    let id : Int
+    let title: String
+    let tag: [String]
+    let city: [String]
+    let writeUser: User
+    let RouteDays: [RouteDay]
+    let start_date: Date
+    let end_date: Date?
+    let created_at: Date
+    let updated_at: Date
+}
