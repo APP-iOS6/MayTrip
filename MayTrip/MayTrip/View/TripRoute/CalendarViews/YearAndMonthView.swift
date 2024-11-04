ba70cd61a5dd8945318c70e6ab04ce0d73d8518f
Leesooooyoung <soyoung3466@gmail.com>
2024-11-04 17:05:39 +0900

    Feat: 연, 월 나타내는 뷰 #4

---
+++ MayTrip/MayTrip/View/TripRoute/CalendarViews/YearAndMonthView.swift
@@ -0,0 +1,46 @@
+//
+//  YearAndMonthView.swift
+//  MayTrip
+//
+//  Created by 이소영 on 11/2/24.
+//
+
+
+import SwiftUI
+
+struct YearAndMonthView: View {
+    var dateStore: DateStore = DateStore.shared
+    
+    var body: some View {
+        HStack {
+            Button {
+                dateStore.forwardMonth()
+            } label: {
+                HStack {
+                    Image(systemName: "chevron.left")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 15, height: 15)
+                }
+                .frame(width: 60, height: 40)
+            }
+            
+            Text("\(String(dateStore.currentYear))년 \(dateStore.currentMonth)월")
+                .font(.system(size: 18))
+            
+            Button {
+                dateStore.backwardMonth()
+            } label: {
+                HStack {
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 15, height: 15)
+                }
+                .frame(width: 60, height: 40)
+            }
+        }
+        .foregroundStyle(.black)
+        .padding(.vertical)
+    }
+}
