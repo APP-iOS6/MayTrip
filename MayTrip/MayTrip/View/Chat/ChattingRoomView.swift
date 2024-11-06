aa9ab92c20e036d7d482349da0ed1bd1cb422e50
Leesooooyoung <soyoung3466@gmail.com>
2024-11-06 11:32:36 +0900

    Fix: 채팅 더미 모델 적용 #15

--- MayTrip/MayTrip/View/Chat/ChattingRoomView.swift
+++ MayTrip/MayTrip/View/Chat/ChattingRoomView.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ChattingRoomView: View {
     @Environment(\.dismiss) var dismiss
     @State var message: String = ""
     @FocusState var focused: Bool
+    var chatStore = DummyChatStore.shared
+    var routeStore = DummyRouteStore.shared
+    let chatRoom: DummyChatRoom
+    var chatLogs: [DummyChatLog] {
+        chatStore.getChatLogs(chatRoom.id)
+    }
+    @State var isScrollTop: Bool = true
+    @State var isSend: Bool = false
     
+    // TODO: 루트 공유
     var body: some View {
         VStack {
             HStack {
@@ -26,7 +36,7 @@ struct ChattingRoomView: View {
                 Spacer()
                 
                 // 채팅 상대 이름
-                Text("김철수")
+                Text("\(chatStore.getOtherUser(chatRoom).nickname)")
                 
                 Spacer()
             }
@@ -37,77 +47,28 @@ struct ChattingRoomView: View {
             Divider()
             
             ScrollView {
-                LazyVStack(alignment: .leading, pinnedViews: [.sectionHeaders]) {
-                    Section(header:
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.gray, lineWidth: 1)
-                                .overlay {
-                                    // storke 쓰면 배경색이 안먹혀서 사각형 한번 더 그려줌
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .foregroundStyle(Color(uiColor: .systemGray6))
-                                        .overlay {
-                                            HStack {
-                                                VStack(alignment: .leading, spacing: 8) {
-                                                    Text("여행 같이 갈 분 구합니다")
-                                                        .lineLimit(1)
-                                                        .bold()
-                                                        
-                                                    HStack(spacing: 10) {
-                                                        // 여행지
-                                                        Text("일본 도쿄")
-                                                        // 여행 날짜, 기간
-                                                        Text("10.29(화) - 11.03(월)")
-                                                        Text("4박 5일")
-                                                    }
-                                                    .font(.system(size: 15))
-                                                    .foregroundStyle(.gray)
-                                                    
-                                                    Text("도쿄로 일본여행 같이 갈 분 구합니다")
-                                                        .lineLimit(1)
-                                                        .font(.system(size: 15))
-                                                        .foregroundStyle(.gray)
-                                                }
-                                                
-                                                Spacer()
-                                            }
-                                            // 현재 여행 중인 카드만 진한 컬러로
-                                            .foregroundStyle(.black)
-                //                            .foregroundStyle(.white)
-                                            .padding()
-                                            .padding(.vertical)
-                                        }
-                                }
+                LazyVStack(alignment: .leading) {
+                    VStack(alignment: .center) {
+                        //                            Text("2024년 11월 04일")
+                        //                                .font(.footnote)
+                        //                                .foregroundStyle(.gray)
+                        //                                .padding()
                         
-                        .padding([.horizontal, .top])
-                        .background {
-                            Rectangle()
-                                .foregroundColor(.white)
-                        }
-                        .padding(.bottom)
-                        .frame(width: .infinity, height: 130)
-                    )
-                    {
-                        VStack(alignment: .center) {
-                            Text("2024년 11월 04일")
-                                .font(.footnote)
-                                .foregroundStyle(.gray)
-                                .padding()
-                            
-                            ForEach(0..<4) { index in
-                                // TODO: 상대가 보낸 메세지인지 분기 로직 추가
-                                
+                        ForEach(chatLogs) { log in
+                            // TODO: 상대가 보낸 메세지인지 분기 로직 추가
+                            if log.write_user == signedUser.id {
                                 // 내가 보낸 메세지
                                 HStack(alignment: .bottom, spacing: 0) {
                                     Spacer()
                                     
                                     VStack(alignment: .trailing) {
-                                        Text("읽음")
-                                        Text("오전 10:45")
+                                        //                                            Text("읽음")
+                                        Text("\(chatStore.convertDateToTimeString(log.created_at))")
                                     }
                                     .font(.footnote)
                                     .foregroundStyle(.gray)
                                     
-                                    Text("안녕하세요. 같이 여행가요. 이 루트로 여행 가는거 어떄요?")
+                                    Text("\(log.message)")
                                         .foregroundStyle(.white)
                                         .padding()
                                         .background {
@@ -118,10 +79,10 @@ struct ChattingRoomView: View {
                                         .padding(.horizontal)
                                 }
                                 .padding(.bottom)
-                                
+                            } else {
                                 // 상대가 보낸 메세지
                                 HStack(alignment: .bottom, spacing: 0) {
-                                    Text("안녕하세요. 같이 여행가는거 너무 좋아요")
+                                    Text("\(log.message)")
                                         .foregroundStyle(.black)
                                         .padding()
                                         .background {
@@ -132,8 +93,8 @@ struct ChattingRoomView: View {
                                         .padding(.horizontal)
                                     
                                     VStack(alignment: .leading) {
-                                        Text("읽음")
-                                        Text("오전 10:45")
+                                        //                                            Text("읽음")
+                                        Text("\(chatStore.convertDateToTimeString(log.created_at))")
                                     }
                                     .font(.footnote)
                                     .foregroundStyle(.gray)
@@ -161,10 +122,18 @@ struct ChattingRoomView: View {
                     }
                     .keyboardType(.default)
                     .focused($focused)
+                    .onSubmit {
+                        chatStore.saveChatLog(chatRoom.id, message: message)
+                        message = ""
+                        focused = false
+                        isSend = true
+                    }
                 
                 Button {
+                    chatStore.saveChatLog(chatRoom.id, message: message)
                     message = ""
                     focused = false
+                    isSend = true
                 } label: {
                     VStack {
                         Image(systemName: "paperplane")
@@ -188,6 +157,63 @@ struct ChattingRoomView: View {
     }
 }
 
+struct RecruitmentNoticeView: View {
+    var routeStore = DummyRouteStore.shared
+    let route: DummyTripRoute?
+    
+    var body: some View {
+        if let route {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.gray, lineWidth: 1)
+                .overlay {
+                    // storke 쓰면 배경색이 안먹혀서 사각형 한번 더 그려줌
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(Color(uiColor: .systemGray6))
+                        .overlay {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 8) {
+                                    Text("\(route.title)")
+                                        .lineLimit(1)
+                                        .bold()
+                                    
+                                    HStack(spacing: 10) {
+                                        // 여행지
+                                        ForEach(0..<3) { index in
+                                            if index < route.cities.count {
+                                                Text("\(index == 0 ? route.cities[index] : "·  \(route.cities[index])")")
+                                                    .lineLimit(1)
+                                            }
+                                        }
+                                        // 여행 날짜, 기간
+                                        Text("\(routeStore.convertPeriodToString(route.start_date, end: route.end_date))여행")
+                                    }
+                                    .font(.system(size: 15))
+                                    .foregroundStyle(.gray)
+                                }
+                                
+                                Spacer()
+                            }
+                            // 현재 여행 중인 카드만 진한 컬러로
+                            .foregroundStyle(.black)
+                            //                            .foregroundStyle(.white)
+                            .padding()
+                            .padding(.vertical)
+                        }
+                }
+            
+                .padding([.horizontal, .top])
+                .background {
+                    Rectangle()
+                        .foregroundColor(.white)
+                }
+                .padding(.bottom)
+                .frame(width: .infinity, height: 130)
+        } else {
+            EmptyView()
+        }
+    }
+}
+
 #Preview {
-    ChattingRoomView()
+    ChattingRoomView(chatRoom: DummyChatStore.shared.chatRooms[0])
 }
