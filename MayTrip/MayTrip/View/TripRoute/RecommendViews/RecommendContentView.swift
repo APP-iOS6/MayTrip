cd50424e1e07e8960c49053537b7e74b2cad3093
Leesooooyoung <soyoung3466@gmail.com>
2024-11-07 17:22:08 +0900

    Design: 추천 부분 전체 디자인 수정 #8

--- MayTrip/MayTrip/View/TripRoute/RecommendViews/RecommendContentView.swift
+++ MayTrip/MayTrip/View/TripRoute/RecommendViews/RecommendContentView.swift
@@ -17,59 +17,62 @@ struct RecommendContentView: View {
             // 디테일 뷰 이동
             RouteDetailView(tripRoute: SampleTripRoute.sampleRoute)
         } label: {
-            VStack(alignment: .leading) {
-                HStack(alignment: .top) {
-                    Text("\(route.title)")
-                        .bold()
-                        .font(.title3)
-                        .multilineTextAlignment(.leading)
-                    
-                    Spacer()
-                    
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "bookmark")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                    }
-                }
-                
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("\(routeStore.convertPeriodToString(route.start_date, end: route.end_date)) 일정")
-                    
-                    HStack {
-                        ForEach(0..<3) { index in
-                            if index < route.cities.count {
-                                Text("\(index == 0 ? route.cities[index] : "·  \(route.cities[index])")")
-                                    .fontWeight(.semibold)
-                                    .lineLimit(1)
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(Color(uiColor: .systemGray6))
+                .overlay(
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .top) {
+                            Text("\(route.title)")
+                                .bold()
+                                .font(.title3)
+                                .multilineTextAlignment(.leading)
+                                .foregroundStyle(.black)
+                            
+                            Spacer()
+                            
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "bookmark")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 20, height: 20)
+                                    .foregroundStyle(Color("accentColor"))
                             }
                         }
-                    }
-                }
-                .padding(.vertical, 10)
-                
-                HStack {
-                    ForEach(0..<3) { index in
-                        if index < route.tags.count {
-                            Text("#\(route.tags[index]) ")
-                                .font(.system(size: 13))
-                                .lineLimit(1)
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("\(routeStore.convertPeriodToString(route.start_date, end: route.end_date)) 일정")
+                            
+                            HStack {
+                                ForEach(0..<3) { index in
+                                    if index < route.cities.count {
+                                        Text("\(index == 0 ? route.cities[index] : "·  \(route.cities[index])")")
+                                            .fontWeight(.semibold)
+                                            .lineLimit(1)
+                                    }
+                                }
+                            }
                         }
+                        .padding(.vertical, 10)
+                        .foregroundStyle(Color(uiColor: .darkGray))
+                        
+                        HStack {
+                            ForEach(0..<3) { index in
+                                if index < route.tags.count {
+                                    Text("#\(route.tags[index]) ")
+                                        .font(.system(size: 13))
+                                        .lineLimit(1)
+                                }
+                            }
+                        }
+                        .font(.footnote)
+                        .foregroundStyle(Color(uiColor: .darkGray))
                     }
-                }
-                .font(.footnote)
-            }
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color(uiColor: .lightGray).opacity(0.3), lineWidth: 2)
-            )
-            .frame(width: 200, height: 200)
+                    .padding()
+                )
+                .frame(width: 240, height: 200)
         }
-        .foregroundStyle(.black)
     }
 }
 
