78581a852cc068acbce7c189bfe5f15897fe2e56
Leesooooyoung <soyoung3466@gmail.com>
2024-11-13 11:54:01 +0900

    Fix: 실제 데이터 적용 #8

--- MayTrip/MayTrip/View/TripRoute/RecommendViews/RecommendRouteView.swift
+++ MayTrip/MayTrip/View/TripRoute/RecommendViews/RecommendRouteView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct RecommendRouteView: View {
-    var routeStore: DummyRouteStore = .shared
+    @StateObject var tripRouteStore: TripRouteStore = TripRouteStore()
+    
+    let dateStore: DateStore = .shared
+    
+    var season: String {
+        dateStore.convertMonthToSeason(Date())
+    }
     
     var body: some View {
         ForEach(Standard.allCases, id: \.self) { standard in
@@ -32,9 +38,11 @@ struct RecommendRouteView: View {
                 
                 ScrollView(.horizontal) {
                     HStack {
-                        ForEach(routeStore.categorizeRoute(standard)) { route in
-                            RecommendContentView(route: route)
-                                .padding(2)
+                        ForEach(0..<5) { index in
+                            if index < categorizedRoute(standard).count {
+                                RecommendContentView(route: categorizedRoute(standard)[index])
+                                    .padding(2)
+                            }
                         }
                     }
                     .padding(.trailing)
@@ -42,10 +50,31 @@ struct RecommendRouteView: View {
             }
             .padding([.leading, .vertical])
         }
+        .onAppear {
+            Task {
+                try await tripRouteStore.getTripRouteList()
+            }
+        }
+    }
+    
+    private func categorizedRoute(_ standard: Standard) -> [TripRouteSimple] {
+        return switch standard {
+        case .bestSeason:
+            tripRouteStore.list.filter {
+                // TODO: 찜을 가장 많이 받은 여행 중에서 계절별로 필터링 하는 로직 추가하기
+                dateStore.convertMonthToSeason(dateStore.convertStringToDate($0.start_date)) == season
+            }
+        case .savedAlot:
+            // TODO: 찜을 가장 많이 받은 여행 필터링하는 로직 추가하기
+            tripRouteStore.list
+        }
     }
 }
 
-
+enum Standard: String, CaseIterable {
+    case savedAlot = "찜을 가장 많이 받은 여행"
+    case bestSeason = "지금 계절에 가기 좋은 여행"
+}
 
 #Preview {
     RecommendRouteView()
