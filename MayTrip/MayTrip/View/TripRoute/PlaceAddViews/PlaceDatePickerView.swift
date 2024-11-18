06d4b38b67576ffed2cf67ae3fb9a627742ab99c
seungho <sts06017@naver.com>
2024-11-18 18:26:38 +0900

    Design: 날짜선택 시트 디자인 변경

--- MayTrip/MayTrip/View/TripRoute/PlaceAddViews/PlaceDatePickerView.swift
+++ MayTrip/MayTrip/View/TripRoute/PlaceAddViews/PlaceDatePickerView.swift
@@ -19,7 +19,8 @@ struct PlaceDatePickerView: View {
         VStack(alignment: .leading) {
             Text("날짜 선택")
                 .font(.title)
-                .padding([.top, .horizontal], 32)
+                .bold()
+                .padding([.top, .horizontal])
             
             ScrollView {
                 ForEach(Array(dateStore.datesInRange().enumerated()), id: \.element) { index, date in
@@ -28,33 +29,37 @@ struct PlaceDatePickerView: View {
                         isShowDatePicker = false
                     } label: {
                         HStack {
-                            Text("Day\(index+1) \(dateStore.convertDateToString(date, format:  "MM.dd(E)"))")
+                            Text("Day \(index+1)")
+                                .font(.headline)
+                                .bold()
                             
-                            if index == focusedDayIndex {
-                                Image(systemName: "checkmark")
-                            }
+                            Spacer()
+                            
+                            Text(dateStore.convertDateToString(date, format: "yyyy년 MM월 dd일(E)"))
                         }
-                        .padding(.vertical, 5)
-                        .padding(.horizontal)
+                        .padding()
+                        .foregroundStyle(getColor(index: index))
+                        .frame(maxWidth: .infinity, alignment: .leading)
                         .background {
-                            if index == focusedDayIndex {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color("accentColor"))
-                            }
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(getColor(index: index))
+                                .opacity(0.1)
                         }
-                        .padding(.leading, 1)
-                        .foregroundStyle(index == focusedDayIndex ? Color.accentColor : .primary)
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.top, 7)
+                    .padding(.bottom, 7)
                 }
             }
             .padding([.horizontal, .bottom])
             .presentationDetents([.medium])
             .presentationDragIndicator(.visible)
         }
+        .padding(.top)
         .onAppear {
             focusedDayIndex = scrollingIndex
         }
+    }
+    
+    private func getColor(index: Int) -> Color {
+        index == focusedDayIndex ? Color("accentColor") : Color(uiColor: .systemGray)
     }
 }
