f85c88529cb9dd3de7d50e09ce978bcbb943fce7
Leesooooyoung <soyoung3466@gmail.com>
2024-11-19 20:22:53 +0900

    Design: 나의여행카드뷰 디자인 수정

--- MayTrip/MayTrip/View/TripRoute/HomeViews/MyTripCardView.swift
+++ MayTrip/MayTrip/View/TripRoute/HomeViews/MyTripCardView.swift
@@ -16,6 +16,7 @@ struct MyTripCardView: View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(tripRouteStore.myTripRoutes.sorted(by: { $0.start_date < $1.start_date })) { route in
+                    // 메인 화면에서는 여행중 또는 앞으로 갈 여행만 보여준다
                     if dateStore.convertStringToDate(route.end_date) >= Date() {
                         Button {
                             Task {
@@ -25,63 +26,65 @@ struct MyTripCardView: View {
                                 }
                             }
                         } label: {
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke((dateStore.isOnATrip(route.start_date, end: route.end_date) ? Color("accentColor") : Color(uiColor: .systemGray4)), lineWidth: 0.5)
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("accentColor"), lineWidth: 1)
                                 .overlay {
-                                    HStack(spacing: 0) {
-                                        VStack(alignment: .leading, spacing: 8) {
-                                            HStack {
-                                                Text("\(route.title)")
-                                                    .font(.title3)
-                                                    .fontWeight(.bold)
-                                                
-                                                Spacer()
-                                            }
-                                            .foregroundStyle(.black)
-                                            
-                                            HStack {
-                                                // 도시는 최대 세개까지만 보여준다
-                                                ForEach(0..<3) { index in
-                                                    if index < route.city.count {
-                                                        Text("\(index == 0 ? route.city[index] : "·  \(route.city[index])")")
-                                                            .lineLimit(1)
-                                                    }
+                                    VStack(alignment: .leading, spacing: 0) {
+                                        HStack {
+                                            // 첫번째 도시만 보여준다
+                                            Text(route.city[0])
+                                                .font(.caption)
+                                                .foregroundStyle(.white)
+                                                .padding(.horizontal, 8)
+                                                .padding(5)
+                                                .background {
+                                                    RoundedRectangle(cornerRadius: 20, style: .circular)
+                                                        .foregroundStyle(.black)
                                                 }
-                                            }
-                                            .fontWeight(.semibold)
                                             
-                                            HStack(spacing: 0) {
-                                                Text("\(dateStore.convertDateToString(dateStore.convertStringToDate(route.start_date), format: "yy.MM.dd"))")
-                                                
-                                                if route.end_date != route.start_date {
-                                                    Text(" - \(dateStore.convertDateToString(dateStore.convertStringToDate(route.end_date), format: "yy.MM.dd"))")
+                                            Spacer()
+                                            
+                                            Text(dateStore.isOnATrip(route.start_date, end: route.end_date) ? "여행중" : dateStore.calcDDay(route.start_date))
+                                                .font(.caption)
+                                                .foregroundStyle(Color("accentColor"))
+                                                .padding(.horizontal, 8)
+                                                .padding(5)
+                                                .background {
+                                                    RoundedRectangle(cornerRadius: 20, style: .circular)
+                                                        .foregroundStyle(Color("accentColor").opacity(0.15))
                                                 }
-                                            }
-                                            .fontWeight(.semibold)
-                                            .lineLimit(1)
                                         }
-                                        .foregroundStyle(Color(uiColor: .darkGray))
-                                        .padding()
-                                        .padding(.vertical)
+                                        .padding(10)
                                         
-                                        Divider()
-                                            .background(.white)
-                                            .padding(.vertical)
+                                        Text(route.title)
+                                            .font(.title3)
+                                            .lineLimit(1)
+                                            .bold()
+                                            .foregroundStyle(.black)
+                                            .padding(.horizontal, 10)
+                                            .multilineTextAlignment(.leading)
                                         
-                                        Text(dateStore.isOnATrip(route.start_date, end: route.end_date) ? "여행중" : dateStore.calcDDay(route.start_date))
-                                            .foregroundStyle(Color("accentColor"))
-                                            .padding(.horizontal)
-                                            .fontWeight(.semibold)
+                                        HStack(spacing: 0) {
+                                            Text("\(dateStore.convertDateToString(dateStore.convertStringToDate(route.start_date), format: "yy.MM.dd"))")
+                                            
+                                            if route.end_date != route.start_date {
+                                                Text(" - \(dateStore.convertDateToString(dateStore.convertStringToDate(route.end_date), format: "yy.MM.dd"))")
+                                            }
+                                        }
+                                        .font(.callout)
+                                        .foregroundStyle(.gray)
+                                        .padding(10)
                                     }
                                 }
-                                .frame(width: UIScreen.main.bounds.size.width / 4 * 3, height: UIScreen.main.bounds.size.height / 8)
+                                .background(.white)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
                         }
-                        .padding(2)
+                        .padding(1)
+                        .frame(minWidth: UIScreen.main.bounds.size.width / 4 * 3, minHeight: UIScreen.main.bounds.size.height / 7.8)
                     }
                 }
             }
-            .padding(.trailing)
+            .padding(.horizontal, 10)
         }
-        .padding(.leading)
     }
 }
