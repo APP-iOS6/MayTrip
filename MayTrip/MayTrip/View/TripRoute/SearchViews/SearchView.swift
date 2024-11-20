032bbaf13bae1c7abccb3bb64ed43b54a5b88a20
Leesooooyoung <soyoung3466@gmail.com>
2024-11-20 13:47:28 +0900

    Feat: 검색탭 완성 #76

--- MayTrip/MayTrip/View/TripRoute/SearchViews/SearchView.swift
+++ MayTrip/MayTrip/View/TripRoute/SearchViews/SearchView.swift
@@ -9,30 +9,48 @@ import SwiftUI
 
 struct SearchView: View {
     @Environment(\.dismiss) var dismiss
+    @StateObject var tripRouteStore: TripRouteStore
     @State var  searchText: String = ""
     @FocusState var focused: Bool
+    @State private var searchList: [String] = []
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             HStack {
                 Button {
+                    tripRouteStore.resetSearchTripRoute()
+                    
                     dismiss()
                 } label: {
                     Image(systemName: "chevron.left")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 15, height:  15)
+                        .foregroundStyle(.black)
                 }
                 .padding(.trailing, 5)
                 
                 HStack {
                     TextField("어디로 떠나시나요?", text: $searchText)
                         .onChange(of: searchText) { oldValue, newValue in
-                            
+                        }
+                        .onSubmit {
+                            Task {
+                                guard searchText != "" else { return }
+                                
+                                tripRouteStore.searchTripRoute(searchText)
+                                
+                                if !searchList.contains(searchText) {
+                                    searchList.insert(searchText, at: 0)
+                                }
+                                UserDefaults.standard.set(searchList, forKey: "searchList")
+                                
+                                searchText = ""
+                                focused = true
+                            }
                         }
                         .keyboardType(.default)
                         .focused($focused)
-                    
                     
                     Button {
                         searchText = ""
@@ -49,21 +67,22 @@ struct SearchView: View {
                     }
                 }
                 .padding(10)
+                .padding(.horizontal, 8)
                 .background {
-                    RoundedRectangle(cornerRadius: 5)
+                    RoundedRectangle(cornerRadius: 20)
                         .stroke(.gray, lineWidth: 1)
                 }
             }
+            .padding(.horizontal)
             
-            RecentlySearchListView()
+            RecentlySearchListView(tripRouteStore: tripRouteStore, searchList: $searchList)
             
-            Spacer()
+            SearchRootView(tripRouteStore: tripRouteStore)
         }
-        .padding()
+        .onAppear {
+            searchList = UserDefaults.standard.array(forKey: "searchList") as? [String] ?? []
+        }
+        .background(Color(uiColor: .systemGray6))
         .navigationBarBackButtonHidden()
     }
-}
-
-#Preview {
-    SearchView()
 }
