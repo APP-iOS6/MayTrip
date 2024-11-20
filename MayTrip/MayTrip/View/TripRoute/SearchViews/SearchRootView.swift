b0bfc27f9142fdfc3ae1d51eefaa07a8c280468f
Leesooooyoung <soyoung3466@gmail.com>
2024-11-20 21:39:14 +0900

    Design: 검색결과 메세지 위치 수정

--- MayTrip/MayTrip/View/TripRoute/SearchViews/SearchRootView.swift
+++ MayTrip/MayTrip/View/TripRoute/SearchViews/SearchRootView.swift
@@ -17,13 +17,8 @@ struct SearchRootView: View {
     var body: some View {
         ScrollView {
             LazyVGrid(columns: gridItems, alignment: .leading, spacing: 10) {
-                if tripRouteStore.filteredTripRoutes.count == 0 {
-                    Text("검색 결과가 없습니다")
-                        .foregroundStyle(.gray)
-                } else {
-                    ForEach(tripRouteStore.filteredTripRoutes) { route in
-                        RecommendContentView(route: route)
-                    }
+                ForEach(tripRouteStore.filteredTripRoutes) { route in
+                    RecommendContentView(route: route)
                 }
             }
         }
--- MayTrip/MayTrip/View/TripRoute/SearchViews/SearchView.swift
+++ MayTrip/MayTrip/View/TripRoute/SearchViews/SearchView.swift
@@ -15,69 +15,76 @@ struct SearchView: View {
     @State private var searchList: [String] = []
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            HStack {
-                Button {
-                    tripRouteStore.resetSearchTripRoute()
-                    
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.left")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 15, height:  15)
-                        .foregroundStyle(.black)
-                }
-                .padding(.trailing, 5)
-                
+        ZStack {
+            VStack(alignment: .leading, spacing: 0) {
                 HStack {
-                    TextField("어디로 떠나시나요?", text: $searchText)
-                        .onChange(of: searchText) { oldValue, newValue in
-                        }
-                        .onSubmit {
-                            Task {
-                                guard searchText != "" else { return }
-                                
-                                tripRouteStore.searchTripRoute(searchText)
-                                
-                                if !searchList.contains(searchText) {
-                                    searchList.insert(searchText, at: 0)
-                                }
-                                UserDefaults.standard.set(searchList, forKey: "searchList")
-                                
-                                searchText = ""
-                                focused = true
-                            }
-                        }
-                        .keyboardType(.default)
-                        .focused($focused)
-                    
                     Button {
-                        searchText = ""
-                        focused = false
+                        tripRouteStore.resetSearchTripRoute()
+                        
+                        dismiss()
                     } label: {
-                        VStack {
-                            if searchText.count > 0 {
-                                Image(systemName: "xmark.circle.fill")
-                            } else {
-                                Image(systemName: "magnifyingglass")
+                        Image(systemName: "chevron.left")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 15, height:  15)
+                            .foregroundStyle(.black)
+                    }
+                    .padding(.trailing, 5)
+                    
+                    HStack {
+                        TextField("어디로 떠나시나요?", text: $searchText)
+                            .onChange(of: searchText) { oldValue, newValue in
                             }
+                            .onSubmit {
+                                Task {
+                                    guard searchText != "" else { return }
+                                    
+                                    tripRouteStore.searchTripRoute(searchText)
+                                    
+                                    if !searchList.contains(searchText) {
+                                        searchList.insert(searchText, at: 0)
+                                    }
+                                    UserDefaults.standard.set(searchList, forKey: "searchList")
+                                    
+                                    searchText = ""
+                                    focused = true
+                                }
+                            }
+                            .keyboardType(.default)
+                            .focused($focused)
+                        
+                        Button {
+                            searchText = ""
+                            focused = false
+                        } label: {
+                            VStack {
+                                if searchText.count > 0 {
+                                    Image(systemName: "xmark.circle.fill")
+                                } else {
+                                    Image(systemName: "magnifyingglass")
+                                }
+                            }
+                            .foregroundStyle(Color(uiColor: .gray))
                         }
-                        .foregroundStyle(Color(uiColor: .gray))
+                    }
+                    .padding(10)
+                    .padding(.horizontal, 8)
+                    .background {
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.gray, lineWidth: 1)
                     }
                 }
-                .padding(10)
-                .padding(.horizontal, 8)
-                .background {
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(.gray, lineWidth: 1)
-                }
+                .padding(.horizontal)
+                
+                RecentlySearchListView(tripRouteStore: tripRouteStore, searchList: $searchList)
+                
+                SearchRootView(tripRouteStore: tripRouteStore)
             }
-            .padding(.horizontal)
             
-            RecentlySearchListView(tripRouteStore: tripRouteStore, searchList: $searchList)
-            
-            SearchRootView(tripRouteStore: tripRouteStore)
+            if tripRouteStore.filteredTripRoutes.count == 0 {
+                Text("검색 결과가 없습니다")
+                    .foregroundStyle(.gray)
+            }
         }
         .onAppear {
             tripRouteStore.resetSearchTripRoute()
