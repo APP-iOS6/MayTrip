f622f022994ad473a78cfd559310de0d7f4cf3d8
Leesooooyoung <soyoung3466@gmail.com>
2024-11-25 09:53:30 +0900

    Design: 여백추가

--- MayTrip/MayTrip/View/Chat/RouteListForChatView.swift
+++ MayTrip/MayTrip/View/Chat/RouteListForChatView.swift
@@ -17,11 +17,12 @@ struct RouteListForChatView: View {
     
     var body: some View {
         ScrollView(.horizontal) {
-            LazyHGrid(rows: gridItems, spacing: 0) {
+            LazyHGrid(rows: gridItems, spacing: 10) {
                 ForEach(routes) { route in
                     RouteCellForChatView(selectedRouted: $selectedRouted, route: route)
                 }
             }
+            .padding(.horizontal, 10)
         }
         .scrollIndicators(.hidden)
     }
