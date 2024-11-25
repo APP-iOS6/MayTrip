c62a3000f22d96445c019085a22d663f6eec964d
Leesooooyoung <soyoung3466@gmail.com>
2024-11-25 18:14:06 +0900

    Fix: 스토리지에서 가져오지 않도록 변경

--- MayTrip/MayTrip/Store/StorageStore.swift
+++ MayTrip/MayTrip/Store/StorageStore.swift
@@ -12,45 +12,57 @@ class StorageStore {
     let userStore = UserStore.shared
     static let shared = StorageStore()
     
-    func uploadImage(images: [UIImage]) async throws -> [String] {
-        var names: [String] = []
-        
-        for image in images {
-            let fileName = "\(Int(Date().timeIntervalSince1970))" + "_" + UUID().uuidString
-            let path = "\(userStore.user.email)/\(fileName).jpg"
-            let data = image.jpegData(compressionQuality: 0.5)
-            
-            
-            if let data = data {
-                do {
-                    try await DB.storage
-                        .from("post_image")
-                        .upload(
-                            "public/\(path)",
-                            data: data
-                        )
-                    names.append("public/\(path)")
-                } catch {
-                    print(error)
-                }
-            }
-        }
-        return names
-    }
+//    func uploadImage(images: [UIImage]) async throws -> [String] {
+//        var names: [String] = []
+//        
+//        for image in images {
+//            let fileName = "\(Int(Date().timeIntervalSince1970))" + "_" + UUID().uuidString
+//            let path = "\(userStore.user.email)/\(fileName).jpg"
+//            let data = image.jpegData(compressionQuality: 0.5)
+//            
+//            
+//            if let data = data {
+//                do {
+//                    try await DB.storage
+//                        .from("post_image")
+//                        .upload(
+//                            "public/\(path)",
+//                            data: data
+//                        )
+//                    names.append("public/\(path)")
+//                } catch {
+//                    print(error)
+//                }
+//            }
+//        }
+//        return names
+//    }
     
-    func getImages(pathes: [String]) async throws -> [UIImage] {
+    static func getImages(pathes: [String]) -> [UIImage] {
         var images: [UIImage] = []
-        do {
-            for path in pathes {
-                let imageData = try await DB.storage.from("post_image").download(path: path)
-                if let image = UIImage(data: imageData){
+        for path in pathes {
+            if let data = Data(base64Encoded: path) {
+                if let image = UIImage(data: data) {
                     images.append(image)
                 }
             }
-        } catch {
-            print(error)
         }
-        
+    
         return images
     }
+//    func getImages(pathes: [String]) async throws -> [UIImage] {
+//        var images: [UIImage] = []
+//        do {
+//            for path in pathes {
+//                let imageData = try await DB.storage.from("post_image").download(path: path)
+//                if let image = UIImage(data: imageData){
+//                    images.append(image)
+//                }
+//            }
+//        } catch {
+//            print(error)
+//        }
+//        
+//        return images
+//    }
 }
